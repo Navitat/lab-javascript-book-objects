175c1c24468743909c4b8090a9a27065fafcaf0e
navitat <anlon636@gmail.com>
2025-06-20 16:48:22 +0200

    solved lab

--- src/books.js
+++ src/books.js
@@ -36,32 +36,69 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
-
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description:
+        "One of Hemingway's most famous works, it tells the story of Santiago...",
+    },
+  },
+  {
+    title: "The Old Man and the Sea",
+    pages: 352,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description:
+        "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+    },
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 128,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description:
+        "Educated is an account of the struggle for self-invention...",
+    },
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description:
+        "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+    },
+  },
+];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
-
-
 
 // Iteration 3 | Delete Language
 // Your code here:
 
-
-
+booksArray.forEach(function (element) {
+  delete element.details.language;
+});
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
-
-
+booksArray.forEach(function (element) {
+  element.readingTime = Math.ceil((element.pages * 500) / 90);
+});
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -69,32 +106,65 @@ function getBookDetails() {
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionary) {
   // Your code here:
-  
+  //const authors = Object.keys(dictionary);
+  let books = [];
+
+  /*Object.keys(dictionary).forEach(function (element) {
+    books.push({ author: element });
+  });*/
+  /*for (const author of Object.values(dictionary)) {
+    books.push(author);
+  }*/
+  for (const author in dictionary) {
+    for (const book of dictionary[author]) {
+      books.push({
+        title: book[0],
+        pages: book[1],
+        author: author,
+      });
+    }
+    /*books.push({
+      title: dictionary[author][0],
+      pages: dictionary[author][1],
+      author: author,
+    });*/
+  }
+
+  //books1.push({ key });
+
+  return books;
 }
 
-
+console.log(booksByAuthor(dictionary));
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(books) {
   // Your code here:
-  
+  let pageCount = 0;
+  let pagesSum = 0;
+  for (const book of books) {
+    pagesSum += book.pages;
+  }
+  pageCount = pagesSum / books.length;
+
+  return pageCount;
 }
